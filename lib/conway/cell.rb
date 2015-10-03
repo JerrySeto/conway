61b2e5043072dc518fce03f77c76df71a82cf0e0
Jerry Seto <jerryseto1@gmail.com>
2015-10-03 15:38:11 -0400

    Add Cell#next_state

--- lib/conway/cell.rb
+++ lib/conway/cell.rb
@@ -11,5 +11,12 @@ module Conway
     def current_state
       alive? ? :alive : :dead
     end
+
+    def next_state(num_neighbours)
+      raise 'invalid number of neighbours' if num_neighbours < 0
+      return :alive if !alive? && num_neighbours == 3
+      return :alive if alive? && (num_neighbours == 2 || num_neighbours == 3)
+      :dead
+    end
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -24,4 +24,38 @@ RSpec.describe Conway::Cell do
       end
     end
   end
+
+  describe '#next_state' do
+    it 'should raise an error if the number of neighbours is less than 0' do
+      cell = Conway::Cell.new
+      expect {cell.next_state(-1)}.to raise_error('invalid number of neighbours')
+    end
+    context 'the cell is alive' do
+      let(:cell) {Conway::Cell.new(alive:true)}
+      it 'should return :dead if it has less than 2 neighbours' do
+        expect(cell.next_state(1)).to eq(:dead)
+        expect(cell.next_state(0)).to eq(:dead)
+      end
+
+      it 'should return :alive if it has exactly 2 or three neighbours' do
+        expect(cell.next_state(2)).to eq(:alive)
+        expect(cell.next_state(3)).to eq(:alive)
+      end
+
+      it 'should return :dead if it has more than 3 neighbours' do
+        expect(cell.next_state(4)).to eq(:dead)
+      end
+    end
+
+    context 'the cell is dead' do
+      let(:cell) {Conway::Cell.new}
+      it 'should return :alive if it has exactly three neighbours' do
+        expect(cell.next_state(3)).to eq(:alive)
+      end
+
+      it 'should return :dead if it does not have exactly three neighbours' do
+        expect(cell.next_state(2)).to eq(:dead)
+      end
+    end
+  end
 end
